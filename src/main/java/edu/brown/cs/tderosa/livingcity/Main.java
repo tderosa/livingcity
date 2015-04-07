2350e810f514b68660af7c85b33f1a8725943ac5
Taylor DeRosa <taylor_derosa@brown.edu>
2015-04-07 17:35:10 -0400

    add src

---
+++ src/main/java/edu/brown/cs/tderosa/livingcity/Main.java
@@ -0,0 +1,15 @@
+package edu.brown.cs.tderosa.livingcity;
+
+public class Main {
+  public static void main(String[] args) {
+    new Main(args).run();
+  }
+
+  private Main(String[] args) {
+  }
+
+  private void run() {
+  }
+
+
+}
